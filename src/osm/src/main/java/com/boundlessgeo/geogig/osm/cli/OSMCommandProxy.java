f24aba45485de81dc7ea0d397db34e0d46f9df24
Erik Merkle <emerkle@bigcajun-2.home.emerkle.net>
2016-09-21 01:59:42 -0500

    Split GeoGig OSM module into it's own repository
    Signed-off-by: Erik Merkle <emerkle@boundlessgeo.com>

---
+++ src/osm/src/main/java/com/boundlessgeo/geogig/osm/cli/OSMCommandProxy.java
@@ -0,0 +1,51 @@
+/* Copyright (c) 2012-2013 Boundless and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Distribution License v1.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/org/documents/edl-v10.html
+ *
+ * Contributors:
+ * Gabriel Roldan (Boundless) - initial implementation
+ */
+package com.boundlessgeo.geogig.osm.cli;
+
+import org.locationtech.geogig.cli.CLICommandExtension;
+
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameters;
+import com.boundlessgeo.geogig.osm.cli.commands.CreateOSMChangeset;
+import com.boundlessgeo.geogig.osm.cli.commands.OSMApplyDiff;
+import com.boundlessgeo.geogig.osm.cli.commands.OSMDownload;
+import com.boundlessgeo.geogig.osm.cli.commands.OSMExport;
+import com.boundlessgeo.geogig.osm.cli.commands.OSMExportPG;
+import com.boundlessgeo.geogig.osm.cli.commands.OSMExportShp;
+import com.boundlessgeo.geogig.osm.cli.commands.OSMHistoryImport;
+import com.boundlessgeo.geogig.osm.cli.commands.OSMImport;
+import com.boundlessgeo.geogig.osm.cli.commands.OSMMap;
+import com.boundlessgeo.geogig.osm.cli.commands.OSMUnmap;
+
+/**
+ * {@link CLICommandExtension} that provides a {@link JCommander} for osm specific commands.
+ * 
+ * @see OSMHistoryImport
+ */
+@Parameters(commandNames = "osm", commandDescription = "GeoGig/OpenStreetMap integration utilities")
+public class OSMCommandProxy implements CLICommandExtension {
+
+    @Override
+    public JCommander getCommandParser() {
+        JCommander commander = new JCommander();
+        commander.setProgramName("geogig osm");
+        commander.addCommand("import-history", new OSMHistoryImport());
+        commander.addCommand("import", new OSMImport());
+        commander.addCommand("export", new OSMExport());
+        commander.addCommand("download", new OSMDownload());
+        commander.addCommand("create-changeset", new CreateOSMChangeset());
+        commander.addCommand("map", new OSMMap());
+        commander.addCommand("unmap", new OSMUnmap());
+        commander.addCommand("export-shp", new OSMExportShp());
+        commander.addCommand("export-pg", new OSMExportPG());
+        commander.addCommand("apply-diff", new OSMApplyDiff());
+        return commander;
+    }
+}
---
+++ src/osm/src/main/java/com/boundlessgeo/geogig/osm/cli/OSMModule.java
@@ -0,0 +1,26 @@
+/* Copyright (c) 2012-2013 Boundless and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Distribution License v1.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/org/documents/edl-v10.html
+ *
+ * Contributors:
+ * Gabriel Roldan (Boundless) - initial implementation
+ */
+package com.boundlessgeo.geogig.osm.cli;
+
+import org.locationtech.geogig.cli.CLIModule;
+
+import com.google.inject.Binder;
+
+/**
+ *
+ */
+public class OSMModule implements CLIModule {
+
+    @Override
+    public void configure(Binder binder) {
+        binder.bind(OSMCommandProxy.class);
+    }
+
+}
